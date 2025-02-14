e9a9c2d4444f0190dfe899304e98281c1b6fdda0
Laevskay <alisa.laevskaya2008@gmail.com>
2025-02-14 19:23:10 +0300

    ggg

--- four.js
+++ four.js
@@ -7,13 +7,34 @@
 
 // зная порядковый номер значение числа из ряда Фибоначчи
 
-let number = 5;
+let number = 10;
 let fib_prev = 0;
 let fib_next;
 let fib = 1;
-for(let i=0; i < number; i++){
+
+if (number === 0) {
+    fib = 0;
+} else if (number == 1) {
+fib = 1;
+} else {
+    for (let i = 1; i < number; i++) {
+        fib_next = fib + fib_prev;
+        fib_prev = fib;
+        fib = fib_next;
+    }
+}
+
+
+for(let i=1; i < number; i++) {
 fib_next = fib + fib_prev;
 fib_prev = fib;
-fib =
+fib =fib_next
 }
 console.log(`Число Фибоначчи = ${fib}`)
+
+for (let i = 0; i<=10; i++) {
+    if(i === 4) {
+        continue;
+    }
+    console.log(`Номер итерации ${i}`)
+}
